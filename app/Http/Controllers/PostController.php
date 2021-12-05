57ff2b2f6a9e9a856412a78a06651f1dde34d090
Abel kabeya <abelkofficial@gmail.com>
2021-12-05 17:11:22 +0200

    Displaying a post from admin - Part 1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     //
+    public function index()
+    {
+
+        return view('admin.posts.index');
+    }
 
     public function show(Post $post)
     {
